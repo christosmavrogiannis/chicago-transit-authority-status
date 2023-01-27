effd0afefcdb326d424a7f0b554829c40cb9b432
Christos Mavrogiannis <christos.mavrogianns@gmail.com>
2023-01-27 15:57:16 +0100

    station object avroProducer procuce method implementation

--- producers/models/station.py
+++ producers/models/station.py
@@ -27,19 +27,13 @@ class Station(Producer):
             .replace("'", "")
         )
 
-        #
-        #
-        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
-        # replicas
-        #
-        #
         topic_name = f"{station_name}" # TODO: Come up with a better topic name
         super().__init__(
             topic_name,
             key_schema=Station.key_schema,
             value_schema=Station.value_schema,
-            # TODO: num_partitions=???,
-            # TODO: num_replicas=???,
+            num_partitions=1,
+            num_replicas=1
         )
 
         self.station_id = int(station_id)
@@ -53,23 +47,14 @@ class Station(Producer):
 
     def run(self, train, direction, prev_station_id, prev_direction):
         """Simulates train arrivals at this station"""
-        #
-        #
-        # TODO: Complete this function by producing an arrival message to Kafka
-        #
-        #
         logger.info("arrival kafka integration incomplete - skipping")
-        #self.producer.produce(
-        #    topic=self.topic_name,
-        #    key={"timestamp": self.time_millis()},
-        #    value={
-        #        #
-        #        #
-        #        # TODO: Configure this
-        #        #
-        #        #
-        #    },
-        #)
+        self.producer.produce(
+            topic=self.topic_name,
+            key={"timestamp": self.time_millis()},
+            value={}, # TODO: Configure this
+            key_schema=Station.key_schema,
+            value_schema=Station.value_schema
+        )
 
     def __str__(self):
         return "Station | {:^5} | {:<30} | Direction A: | {:^5} | departing to {:<30} | Direction B: | {:^5} | departing to {:<30} | ".format(
