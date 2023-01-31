4f37111786f5588329dd59221bf9c1532f1c743a
Christos Mavrogiannis <christos.mavrogianns@gmail.com>
2023-01-31 21:30:58 +0100

    weather producer

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -20,9 +20,10 @@ class Turnstile(Producer):
             .replace("-", "_")
             .replace("'", "")
         )
-
+        
+        topic_name = f"turnstile.{station_name}", # TODO: Come up with a better topic name
         super().__init__(
-            f"turnstile.{station_name}", # TODO: Come up with a better topic name
+            topic_name,
             key_schema=Turnstile.key_schema,
             value_schema=Turnstile.value_schema,
             num_partitions=1,
--- producers/tempo.py
+++ producers/tempo.py
@@ -6,3 +6,13 @@ broker_properties = {
 }
 
 print(broker_properties['config']['BROKER_URL'])
+
+from pathlib import Path
+import json
+with open(f"{Path(__file__).parents[0]}/models/schemas/weather_key.json") as f:
+    key_schema = json.load(f)
+
+print(type(key_schema))
+
+key_schema_in_string=json.dumps(key_schema)
+print(type(key_schema_in_string))
