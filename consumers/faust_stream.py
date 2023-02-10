3d00af35e95113e570e6c45db1e92ebbe751859a
Christos Mavrogiannis <christos.mavrogianns@gmail.com>
2023-02-10 22:03:37 +0100

    Fix - change faust stream output topic name

--- consumers/faust_stream.py
+++ consumers/faust_stream.py
@@ -52,7 +52,7 @@ app = faust.App("stations-stream", broker="kafka://localhost:9093", store="memor
 # Define the input Kafka Topic to which Kafka Connect outputs
 topic = app.topic("connect-stations", value_type=Station)
 # Define the output Kafka Topic
-out_topic = app.topic("connect-stations-transformed", partitions=1,  value_type=TransformedStation)
+out_topic = app.topic("org.chicago.cta.stations.table.v1", partitions=1,  value_type=TransformedStation)
 
 
 
